10e7c39972bde160762968eef1e846f68693ef96
Jack Sarick <jacksarick@gmail.com>
2016-10-22 19:06:08 -0400

    Save Sat 22 Oct 2016 19:06:08 EDT

--- click.js
+++ click.js
@@ -29,7 +29,7 @@ $(document).ready(function(){
           $(".loading").remove();
           $(".list-group").append(`<a onclick="showit('#${data[i].id}')" class="message" href="#">
             <li class="list-group-item mail-item">
-            <table><tr>
+            <table style="width: 100%;"><tr style="width: 100%;">
               <td>${data[i].title}<td>
               <td>${data[i].sender}</td>
               <td><span class="tag tag-primary tag-pill float-xs-right">${data[i].date}</span></td>
