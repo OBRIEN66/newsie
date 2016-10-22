4ae6ebdf2a7de3198783099adb03bca08191e8ee
Jack Sarick <jacksarick@gmail.com>
2016-10-22 18:43:16 -0400

    Save Sat 22 Oct 2016 18:43:16 EDT

--- click.js
+++ click.js
@@ -1,9 +1,16 @@
 function showit(id) {
   $(id).show();
+  $(".top").hide();
+}
+
+function hideit(id) {
+  $(id).hide();
+  $(".top").show();
 }
 
 $(document).ready(function(){
   $(".inbox-title").hide();
+  $(".loading").hide();
   $(".button").click(function(){
       $.post("http://newsie.club/data",
       {
@@ -18,20 +25,23 @@ $(document).ready(function(){
         var data = JSON.parse(data);
         console.log(data);
         for (var i = 0; i < data.length; i++) {
-          $(".loading").hide();
+          $(".loading").remove();
           $(".list-group").append(`<a onclick="showit(${data[i].id})" class="message" href="#">
             <li class="list-group-item mail-item">
               ${data[i].title} <span class="sender">${data[i].sender}</span><span class="tag tag-primary tag-pill float-xs-right">${data[i].date}</span>
             </li>
           </a>`);
           $(".total").append(`
-            <div id="${data[i].id}">
+            <div id="${data[i].id}" class="email-content">
               <h3>${data[i].sender}</h3>
               <h4>${data[i].title}</h4>
               <span class="tag tag-pill tag-primary">${data[i].date}</span>
               <p>
                 ${data[i].body}
               </p>
+              </div>
+                <a class="done btn btn-primary" onclick="hideit(${data[i].id})">Done? Head home</a>
+              </div>
             </div>
           `);
         }
